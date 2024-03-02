5ca31ddaafdbfbb78aece95617f298e03bab4cd2
cage.a80.wsl <bea2k@ya.ru>
2024-03-01 22:29:44 -0300

    #hotfix properly handle fedex responses without pricing data

--- src/FedEx/Services/Rates/RatesResponse.php
+++ src/FedEx/Services/Rates/RatesResponse.php
@@ -18,30 +18,35 @@ class RatesResponse
 
     public function details(string $type = null): array
     {
-        return array_map(
+        return array_values(array_filter(array_map(
             function (array $service) use ($type) {
-                $price = Arr::first($service['ratedShipmentDetails'], function (array $details) use ($type) {
-                    return !$type || $details['rateType'] == $type;
-                });
-
-                return array_merge([
-                    'serviceType' => $service['serviceType'],
-                    'serviceName' => $service['serviceName'],
-                    'rateType' => $price['rateType'],
-                    'charges' => [
-                        'base' => $price['totalBaseCharge'],
-                        'surcharge' => Arr::get($price, 'shipmentRateDetail.totalSurcharges'),
-                        'discounts' => $price['totalDiscounts'],
-                        'total' => $price['totalNetCharge'],
-                        'currency' => $price['currency']
-                    ],
-                    'shipmentDetails' => $price,
-                ],
-                    Arr::get($this->config, 'ratesRaw', false) ? ['raw' => $service] : [],
-                    isset($service['commit']) ? ['transitTime' => $service['commit']] : []
+                $price = Arr::first(
+                    Arr::get($service, 'ratedShipmentDetails', []),
+                    function (array $details) use ($type) {
+                        return !$type || $details['rateType'] == $type;
+                    }
                 );
+
+                return !empty($price)
+                    ? array_merge([
+                        'serviceType' => $service['serviceType'],
+                        'serviceName' => $service['serviceName'],
+                        'rateType' => $price['rateType'],
+                        'charges' => [
+                            'base' => $price['totalBaseCharge'],
+                            'surcharge' => Arr::get($price, 'shipmentRateDetail.totalSurcharges'),
+                            'discounts' => $price['totalDiscounts'],
+                            'total' => $price['totalNetCharge'],
+                            'currency' => $price['currency']
+                        ],
+                        'shipmentDetails' => $price,
+                    ],
+                        Arr::get($this->config, 'ratesRaw', false) ? ['raw' => $service] : [],
+                        isset($service['commit']) ? ['transitTime' => $service['commit']] : []
+                    )
+                    : null;
             },
             Arr::get($this->data, 'output.rateReplyDetails', [])
-        );
+        )));
     }
 }
